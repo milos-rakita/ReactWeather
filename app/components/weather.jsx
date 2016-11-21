d0078237b3125dd03f43a499cd203cf16ad58450
milos-rakita <milossrb.rakita@gmail.com>
2016-11-21 15:36:13 +0100

    q

--- app/components/weather.jsx
+++ app/components/weather.jsx
@@ -15,8 +15,9 @@ var Weather = React.createClass({
 
         this.setState({
             isLoading: true,
-            errorMessage: undefined
-
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         }); 
 
         openWeatherMap.getTemp(location).then(function(temp){
@@ -30,8 +31,15 @@ var Weather = React.createClass({
                 isLoading:false,
                 errorMessage: e.message
             });
-            // alert(errorMessage);
         });
+    },
+    componentDidMount: function(){
+        var location = this.props.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
     },
     render : function(){
         var{isLoading,temp,location,errorMessage}=this.state;
